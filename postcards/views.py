7b71246dd4544c8dbde928fc55e75db947040ecf
Antonio Ladeia <contato@antonioladeia.com>
2021-07-03 20:57:02 -0300

    add total distance and time

--- postcards/views.py
+++ postcards/views.py
@@ -4,9 +4,15 @@ from django.shortcuts import render
 
 def index(request):
     postcards = Postcard.objects.all()
+    total_distance = 0
+    total_days = 0
     for postcard in postcards:
-        setattr(postcard, 'avg', int(postcard.distance.replace(',', '')) / int(postcard.time_travel))
+        distance = int(postcard.distance.replace(',', ''))
+        days = int(postcard.time_travel)
+        total_distance += distance
+        total_days += days
+        setattr(postcard, 'avg', distance / days)
 
-    context = {"postcards": postcards}
+    context = {"postcards": postcards, "total_distance": total_distance, "total_days": total_days}
 
     return render(request, 'index.html', context)
