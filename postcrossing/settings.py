3cb252a816092b05bae03ece992c4d4f85543819
Antonio Ladeia <contato@antonioladeia.com>
2021-07-04 07:39:19 -0300

    redo postgres db configuration

--- postcrossing/settings.py
+++ postcrossing/settings.py
@@ -134,3 +134,5 @@ STATICFILES_DIRS = (
 )
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
