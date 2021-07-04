5b167e5b2ce853d1ef5c01418aa939765b355143
Antonio Ladeia <contato@antonioladeia.com>
2021-07-04 06:11:52 -0300

    new stats

--- postcards/views.py
+++ postcards/views.py
@@ -13,6 +13,15 @@ def index(request):
         total_days += days
         setattr(postcard, 'avg', distance / days)
 
-    context = {"postcards": postcards, "total_distance": total_distance, "total_days": total_days}
+    avg_time = total_days / len(postcards)
+    avg_distance = total_distance / len(postcards)
+
+    context = {"postcards": postcards,
+               "total_distance": total_distance,
+               "total_days": total_days,
+               "avg_total": total_distance / total_days,
+               "avg_time": avg_time,
+               "avg_distance": avg_distance
+               }
 
     return render(request, 'index.html', context)
