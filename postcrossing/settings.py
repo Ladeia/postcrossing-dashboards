5a1af6e58a540e90366ee7c78c7e4db08778b569
Antonio Ladeia <contato@antonioladeia.com>
2021-07-04 07:22:31 -0300

    back to sqlite db

--- postcrossing/settings.py
+++ postcrossing/settings.py
@@ -134,5 +134,3 @@ STATICFILES_DIRS = (
 )
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
